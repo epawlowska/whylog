8f7ed36db0b962446b0a1886197126f9850b32c3
epawlowska <epawlowska@users.noreply.github.com>
2016-04-15 21:50:41 +0200

    AbstractConstraint improved

--- whylog/constraints/__init__.py
+++ whylog/constraints/__init__.py
@@ -1,11 +1,52 @@
-from abc import ABCMeta
+from abc import ABCMeta, abstractmethod
 
 import six
+
+from whylog.constraints.const import ConstraintType
+from whylog.constraints.exceptions import ConstraintVerificationError, ConstructorParamsError
+from whylog.teacher.user_intent import UserConstraintIntent
 
 
 @six.add_metaclass(ABCMeta)
 class AbstractConstraint(object):
-    pass
+
+    type = 'undefined'
+
+    @abstractmethod
+    def __init__(self, param_dict, groups):
+        """
+        Creates constraint generally for Teacher and Front use.
+
+        :param param_dict: dict of additional params of constraint
+        :param groups: list of tuples (line_id, group_no),
+                       where line_id and group_no is inner numeration between Front and Teacher
+        """
+        param_dict_keys = param_dict.keys()
+        correct_param_names = self.get_param_names()
+        if not param_dict_keys == correct_param_names:
+            raise ConstructorParamsError(correct_param_names, param_dict_keys)
+        self.params = param_dict
+        self.groups = groups
+
+    @abstractmethod
+    def convert_to_user_constraint_intent(self):
+        return UserConstraintIntent(self.type, self.groups, self.params)
+
+    @classmethod
+    def get_param_names(cls):
+        """
+        Returns names of constraint params.
+
+        This method is for Front, who displays to user param names and asks user for param contents.
+        """
+        raise NotImplementedError("Subclass should implement this")
+
+    @classmethod
+    def verify(cls, param_dict, group_contents):
+        """
+        Verifies constraint for given params and groups contents.
+
+        """
 
 
 class TimeConstraint(AbstractConstraint):
--- whylog/constraints/exceptions.py
+++ whylog/constraints/exceptions.py
@@ -11,3 +11,29 @@ class UnsupportedConstraintTypeError(VerificationError):
 
     def __str__(self):
         return "No such constraint (%s) registered" % self._constraint_data['name']
+
+class ConstraintError(WhylogError):
+    pass
+
+
+class ConstraintVerificationError(ConstraintError):
+    def __init__(self, constr_type, param_dict, group_contents):
+        self.constr_type = constr_type
+        self.param_dict = param_dict
+        self.group_contents = group_contents
+
+    def __str__(self):
+        return 'Constraint verification failed, constraint type: %s, params dict: %s, group contents %s' % (
+            self.constr_type, self.param_dict, self.group_contents
+        )
+
+
+class ConstructorParamsError(ConstraintError):
+    def __init__(self, correct_param_names, incorrect_param_names):
+        self.correct_params_names = correct_param_names
+        self.incorrect_params_names = incorrect_param_names
+
+    def __str__(self):
+        return 'Incorrect keys in constructor params dict, actual keys: %s, should be: %s' % (
+            self.incorrect_params_names.keys(), self.correct_params_names.keys()
+        )
