26b98ef301a771728eebaf7b957daec5ec727a86
epawlowska <epawlowska@users.noreply.github.com>
2016-04-19 00:05:16 +0200

    Review fixes

--- whylog/constraints/exceptions.py
+++ whylog/constraints/exceptions.py
@@ -6,23 +6,39 @@ class ConstraintError(WhylogError):
 
 
 class ConstraintVerificationError(ConstraintError):
-    def __init__(self, constr_type, param_dict, group_contents):
-        self.constr_type = constr_type
+    def __init__(self, constraint_type, param_dict, group_contents):
+        self.constraint_type = constraint_type
         self.param_dict = param_dict
         self.group_contents = group_contents
 
     def __str__(self):
         return 'Constraint verification failed, constraint type: %s, params dict: %s, group contents %s' % (
-            self.constr_type, self.param_dict, self.group_contents
+            self.constraint_type, self.param_dict, self.group_contents
         )
 
 
 class ConstructorParamsError(ConstraintError):
-    def __init__(self, correct_param_names, incorrect_param_names):
+    def __init__(self, constraint_type, correct_param_names, incorrect_param_names):
+        self.constraint_type = constraint_type
         self.correct_params_names = correct_param_names
         self.incorrect_params_names = incorrect_param_names
 
     def __str__(self):
-        return 'Incorrect keys in constructor params dict, actual keys: %s, should be: %s' % (
-            self.incorrect_params_names.keys(), self.correct_params_names.keys()
+        return 'Incorrect keys in constructor params dict in constraint: %s, actual keys: %s, should be: %s' % (
+            self.constraint_type, self.incorrect_params_names.keys(),
+            self.correct_params_names.keys()
+        )
+
+
+class ConstructorGroupsError(ConstraintError):
+    def __init__(self, constraint_type, groups_count, minimal_groups_count, maximal_groups_count):
+        self.constraint_type = constraint_type
+        self.groups_count = groups_count
+        self.minimal_groups_count = minimal_groups_count
+        self.maximal_groups_count = maximal_groups_count
+
+    def __str__(self):
+        return 'Incorrect groups count in constraint: %s, has %s groups, should be at least: %s, at most %s' % (
+            self.constraint_type, self.groups_count, self.minimal_groups_count,
+            self.maximal_groups_count
         )
